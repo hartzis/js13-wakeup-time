82470d68846c7f833d8fe03217846dec1aecf0a0
Brian Emil Hartz <brianhartz@gmail.com>
2014-04-15 16:28:29 -0600

    initial commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).on('ready', function() {
+
+    var outerShell = $('<div>', {
+        class: 'outer-shell',
+    })
+
+
+    var innerShell = $('<div>', {
+        class: 'inner-shell',
+    })
+
+    var innerShell = $('<div>', {
+        class: 'inner-shell',
+    })
+
+
+
+
+    $('.clock-container').append(outerShell.append(innerShell));
+
+});
